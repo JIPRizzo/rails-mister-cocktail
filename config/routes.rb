bcb9c5908b4fcb5e146d79c47e3d72f4dd50588e
joseportillo <jiprizzo@gmail.com>
2017-05-19 12:47:58 +0200

    New Seed File & Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 # only: or except: to limit the user with the options you want him
 # or her to have.
-  resources :cocktails, only: [: ]
-  resources :doses, only: [: ]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:show, :index, :new, :create ]
+  resources :doses, only: [:create ]
 end
+
+#     resources :doses, only: [:destroy]
+#     root "cocktails#index"
+# end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
 
 # create
@@ -16,4 +20,3 @@ end
 # show
 # new
 # update
-

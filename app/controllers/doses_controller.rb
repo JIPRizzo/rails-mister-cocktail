2e30c581107987890a229892b8fdedab705fe445
joseportillo <jiprizzo@gmail.com>
2017-05-19 14:39:08 +0200

    Version 1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,24 @@
 class DosesController < ApplicationController
-end
 
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @dose.description = params[:dose][:description]
+    @dose.ingredient = Ingredient.find(params[:dose][:ingredient])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render "cocktails/show"
+    end
+  end
 
-
-
-
-private
+  private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient)
   end
+
 end
+
+
